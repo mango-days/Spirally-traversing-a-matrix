1e47b26260f38e542b0deeb6f11b7d69c300386e
mango-days <77330707+mango-days@users.noreply.github.com>
2021-12-13 22:58:04 +0530

    Update spiral_traversal.py

--- spiral_traversal.py
+++ spiral_traversal.py
@@ -6,11 +6,16 @@ matrix = [
     ]
     
 index1 = 0
+
 while True :
+            
     if index1 == 0 :
+        
         for index2 in range ( 0 , len ( matrix [ index1 ] ) ) :
             print ( matrix [ index1 ] [ index2 ] )
+        
         matrix .pop ( index1 )
+        
         while index1 != len ( matrix ) - 1 :
             temp = len ( matrix [ index1 ] ) -1
             print ( matrix [ index1 ] [ temp ] )
@@ -20,15 +25,21 @@ while True :
         
     if index1 == len ( matrix ) - 1 :
         index2 = len ( matrix [ index1 ] ) - 1
+        
         while matrix [ index1 ] :
             print ( matrix [ index1 ] [ index2 ] )
             matrix [ index1 ] .pop ( index2 )
             index2 -= 1
+        
         matrix .pop ( index1 )
         index1 -= 1
-        while matrix :
+        
+        while index1 > -1 :
             print ( matrix [ index1 ] [ 0 ] )
             matrix [ index1 ] .pop ( 0 )
             index1 -= 1
+        
         index1 = 0
+        
     if len ( matrix ) == 0 : break
+
